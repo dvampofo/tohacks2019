d80147b5477c2b01335407660298c6293fc82a43
Nishkrit Desai <nishkritdesai10@gmail.com>
2019-06-22 20:56:50 -0400

    Refactored cost analysis code

--- application.py
+++ application.py
@@ -1,37 +1,7 @@
-from flask import Flask, request, jsonify
-import pandas as pd
-import datetime
+from flask import Flask, request
+from CalculateCost import analyse_cost
 
 app = Flask(__name__)
-
-def analyze(file):
-    data = pd.read_csv(file)
-    # Coerce date to datetime 
-    data['Date'] = pd.to_datetime(data['Date'])
-    data.sort_values(by=['Date'], axis=0, ascending=True, inplace=True)
-
-    start_datetime = datetime.datetime(2019,1,1,0,0,0,0)
-    unique_trips = 0
-    for index, row in data.iterrows():
-        agency = row['Transit Agency']
-        type = row['Type ']
-        curr_datetime = row['Date']
-        location = row['Location']
-
-        if agency == "Toronto Transit Commission" and (type == "Transit Pass Payment" or type == "Fare Payment"):
-            diff = abs(curr_datetime - start_datetime)
-            difference = diff.days * 86400 + diff.seconds
-            
-            if (difference > 7200): # time between trips is greater than two hours
-                start_datetime = curr_datetime
-                print("----UNIQUE TRIP----")
-                print("Difference", diff)
-                unique_trips += 1
-            
-            print(curr_datetime, location)
-
-    print("Unique trips", unique_trips)
-    return jsonify({"Unique trips":unique_trips})
 
 @app.route('/test', methods=['POST'])
 def test():
@@ -42,7 +12,7 @@ def upload():
     if 'file' in request.files:
         file = request.files['file']
         # print(file)
-        result = analyze(file)
+        result = analyse_cost(file)
         return result
 
 if __name__ == '__main__':
---
+++ constants.py
@@ -0,0 +1,6 @@
+#! usr/bin/env python
+# -*- coding: utf-8 -*-
+
+TRANSIT_COMMISSION = 'Toronto Transit Commission'
+SEC_IN_A_DAY = 24 * 60 * 60
+TWO_HOURS = 2 * 60 * 60
