e1a95d8a2051cfa5f206b98e956e728cbe4b3fe0
Nishkrit Desai <nishkritdesai10@gmail.com>
2019-06-23 00:31:33 -0400

    handled json requests

--- application.py
+++ application.py
@@ -20,7 +20,8 @@ def upload():
     if 'file' in request.files:
         file = request.files['file']
         # print(file)
-        result = analyse_cost(file)
+        result = json.dumps(analyse_cost(file))
+        print(result)
 
         return redirect(url_for('.get_summary', result=result))
 
@@ -28,7 +29,7 @@ def upload():
 @app.route('/summary')
 def get_summary():
     result = request.args['result']
-    return render_template('summary.html', result=result)
+    return render_template('summary.html', result=json.loads(result))
 
 
 if __name__ == '__main__':
