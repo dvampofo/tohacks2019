6c3a893a3c3f2c12fc87f804a16a1e69fb80754f
Nishkrit Desai <nishkritdesai10@gmail.com>
2019-06-23 10:55:41 -0400

    fixed redirect issue

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 from CalculateCost import analyse_cost
 import json
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 
 @app.route('/')
@@ -20,17 +22,18 @@ def upload():
     if 'file' in request.files:
         file = request.files['file']
         # print(file)
-        result = json.dumps(analyse_cost(file))
-        print(result)
+        result_cost = analyse_cost(file)
+        session['result'] = result_cost
+        print(result_cost)
 
-        return redirect(url_for('.get_summary', result=result))
+        return redirect(url_for('.get_summary'))
 
 
 @app.route('/summary')
 def get_summary():
-    result = request.args['result']
+    result = session['result']
     return render_template('summary.html', result=json.loads(result))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=8000)
