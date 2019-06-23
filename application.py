34fd1c4cee3c8f053bbecf8ce6241be807166b66
Nishkrit Desai <nishkritdesai10@gmail.com>
2019-06-23 00:02:32 -0400

    added summary page

--- application.py
+++ application.py
@@ -1,15 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from CalculateCost import analyse_cost
+import json
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
+
 @app.route('/test', methods=['POST'])
 def test():
     return "Sucess Azure deployment"
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -17,7 +21,15 @@ def upload():
         file = request.files['file']
         # print(file)
         result = analyse_cost(file)
-        return result
+
+        return redirect(url_for('.get_summary', result=result))
+
+
+@app.route('/summary')
+def get_summary():
+    result = request.args['result']
+    return render_template('summary.html', result=json.loads(result))
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
